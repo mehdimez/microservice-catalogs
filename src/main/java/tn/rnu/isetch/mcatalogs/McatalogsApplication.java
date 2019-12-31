ef1bd7097f879dbd551e191fd7e9ab689eb62d77
Mehdi Mezghanni <mahdimez@gmail.com>
2019-12-31 14:26:34 +0100

    Empty Project

---
+++ src/main/java/tn/rnu/isetch/mcatalogs/McatalogsApplication.java
@@ -0,0 +1,13 @@
+package tn.rnu.isetch.mcatalogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McatalogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McatalogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/rnu/isetch/mcatalogs/McatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.rnu.isetch.mcatalogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
