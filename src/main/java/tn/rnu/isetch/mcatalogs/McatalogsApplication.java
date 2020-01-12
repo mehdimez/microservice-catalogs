09b32760c48ef1d833894fba4385244b43a11dd0
azizbo <azizbourguiba.ab@gmail.com>
2020-01-13 00:45:42 +0100

    look chtar

--- src/main/java/tn/rnu/isetch/mcatalogs/McatalogsApplication.java
+++ src/main/java/tn/rnu/isetch/mcatalogs/McatalogsApplication.java
@@ -1,12 +1,23 @@
 package tn.rnu.isetch.mcatalogs;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import tn.rnu.isetch.mcatalogs.entity.Product;
+import tn.rnu.isetch.mcatalogs.repository.ProductRepository;
+import tn.rnu.isetch.mcatalogs.service.ProductService;
+import tn.rnu.isetch.mcatalogs.service.ProductServiceImpl;
+
+import javax.xml.ws.Service;
 
 @SpringBootApplication
-public class McatalogsApplication {
+public class McatalogsApplication implements CommandLineRunner {
+
+	@Autowired
+	ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McatalogsApplication.class, args);
@@ -17,4 +28,27 @@ public class McatalogsApplication {
 		return new ModelMapper();
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		productRepository.deleteAll();
+
+	Product	p1 = new Product() ;
+		Product	p2 = new Product() ;
+		Product	p3 = new Product() ;
+		Product	p4 = new Product() ;
+
+		Product	look = new Product() ;
+		Product	look1 = new Product() ;
+		look.setName("classic");
+		look1.setName("chic");
+
+		p2.setLook(look);
+		p4.setLook(look);
+productRepository.save(look);
+		productRepository.save(p2);
+		productRepository.save(p4);
+
+
+
+	}
 }
--- src/main/java/tn/rnu/isetch/mcatalogs/dto/ProductDto.java
+++ src/main/java/tn/rnu/isetch/mcatalogs/dto/ProductDto.java
@@ -1,6 +1,9 @@
 package tn.rnu.isetch.mcatalogs.dto;
 
+import tn.rnu.isetch.mcatalogs.entity.Product;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class ProductDto implements Serializable {
     private static final long serialVersionUID = 1L;
@@ -14,6 +17,11 @@ public class ProductDto implements Serializable {
     private double vat ;
     private double priceExclTax ;
     private double priceInclTax;
+
+    private List<ProductDto> productList ;
+
+    private   ProductDto look ;
+
 
     public Long getId() {
         return id;
@@ -94,5 +102,22 @@ public class ProductDto implements Serializable {
 
     public void setPicture(String picture) {
         this.picture = picture;
+    }
+
+
+    public List<ProductDto> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<ProductDto> productList) {
+        this.productList = productList;
+    }
+
+    public ProductDto getLook() {
+        return look;
+    }
+
+    public void setLook(ProductDto look) {
+        this.look = look;
     }
 }
