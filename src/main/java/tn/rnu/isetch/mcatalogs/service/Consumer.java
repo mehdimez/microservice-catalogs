0fc016e0138f14a27fb8deb35343ee89b23958b8
Mehdi Mezghanni <mahdimez@gmail.com>
2020-01-15 21:14:58 +0100

    kafka dependency fixed

--- src/main/java/tn/rnu/isetch/mcatalogs/service/Consumer.java
+++ src/main/java/tn/rnu/isetch/mcatalogs/service/Consumer.java
@@ -2,6 +2,7 @@ package tn.rnu.isetch.mcatalogs.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
